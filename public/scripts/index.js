64fa031d44caeb95481818be6923f25b75180334
jimmijazz <joshua.bitossi@gmail.com>
2017-01-06 16:33:55 +1000

    heroku push

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,20 +2,7 @@
 var base_url = "https://hiliteapp.herokuapp.com";
 var userid = "16b8be7599b43835e5dafd448de630664b4cacf551f7ad18ee49ca094cdcc44";
 
-
-var getItems = function(userID) {
-  $.post(
-      base_url + "/links",
-      {"id" : userID},
-      function(err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(data);
-        }
-  })
-};
-
+// Contains all the item cards and loads list data
 var ItemBox = React.createClass({
   getItems : function() {
     $.post(
@@ -34,7 +21,7 @@ var ItemBox = React.createClass({
   },
   componentDidMount: function() {
     this.getItems();
-    setInterval(this.getItems, this.props.pollInterval);
+    // setInterval(this.getItems, this.props.pollInterval);
   },
   render: function() {
     return (
@@ -45,6 +32,7 @@ var ItemBox = React.createClass({
   }
 });
 
+// List of each of the item cards
 var itemList = React.createClass({
   render: function() {
     var listNodes = this.props.data.map(function(item) {
