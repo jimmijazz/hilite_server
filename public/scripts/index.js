9323a9ca3227bd8e819ff39f7dce5ec8635cfdee
jimmijazz <joshua.bitossi@gmail.com>
2017-01-07 13:21:29 +1000

    heroku push

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -12,7 +12,7 @@ var ItemBox = React.createClass({
         base_url + "/getitems",
         {"id" : this.props.userID},
         function(data, status) {
-          console.log(data.data.items.items);
+          console.log("data",data.data.items.items);
           this.setState({data: data.data.items.items});
     }.bind(this)
   ).fail(function() {
