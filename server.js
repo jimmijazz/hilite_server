0ffc45460274ef99d52dc3e71d66df8e306c51d8
jimmijazz <joshua.bitossi@gmail.com>
2017-01-07 16:05:49 +1000

    heroku push

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var insertItem = function(db, content, callback) {
   db.collection(POSTS).update(
     { _id : content._id},
     {$push:
-      { items : content }
+      { items : item }
     },
     { upsert : true },
     function(err) {
