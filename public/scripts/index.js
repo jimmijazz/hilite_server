da6ac4a1994a2f2048306a9163d7eb22cc5b1d34
jimmijazz <joshua.bitossi@gmail.com>
2017-01-07 15:33:19 +1000

    heroku push

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -39,9 +39,7 @@ var ItemBox = React.createClass({
 
 // List of each of the item cards
 var ItemList = React.createClass({
-  componentDidMount: function() {
-    console.log(this.props.data);
-  }
+
   // Delete items
   handleDelete: function(commentId) {
     // console.log(this.item_id);
